52e3ce9f6475280bc12b53a4d1d4f6610407fd01
Hutuan6886 <tuanlehuu72@gmail.com>
2024-05-20 03:28:21 +0700

    6. Set-up redux-persist to save the user info on the default localStorage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "@/features/user/userSlice"; //* export toàn bộ @/features/user/userSlice đặt tên là UserReducer
+import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage"; //* defaults to localStorage for web
+import { persistReducer, persistStore } from "redux-persist";
+
+//todo: Tạo redux-persist để lưu data vào localStorage
+const rootReducer = combineReducers({
+  //todo: Khai báo toàn bộ reducer của app
+  user: UserReducer,
+});
+const persistConfig = {
+  //todo: Key name, version and storage of localStorage
+  key: "root",
+  storage, //* defaults to localStorage for web
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: UserReducer,
-  },
+  // reducer: {
+  //   //*: Khai báo toàn bộ reducer của app khi không cấu hình redux-persist
+  //   user: UserReducer,
+  // },
+  reducer: persistedReducer,  //* reducer khi cấu hình redux-persist
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store)  //* export persistStore  
